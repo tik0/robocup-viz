9701fe9d571705f98b0fd8f8f0e8dce9e53d233a
Timo Korthals <tkorthals@techfak.uni-bielefeld.de>
2016-06-29 16:25:02 +0200

    com_viz: Add threading which handles the visualization; rviz: Add communication arrows and waypoint beacons

--- catkin_ws/src/com_viz/src/main.cpp
+++ catkin_ws/src/com_viz/src/main.cpp
@@ -2,7 +2,6 @@
 #include <tf/transform_listener.h>
 #include <visualization_msgs/Marker.h>
 
-
 #include <rsb/Factory.h>
 #include <rsb/Version.h>
 #include <rsb/Event.h>
@@ -11,38 +10,27 @@
 #include <rsb/converter/ProtocolBufferConverter.h>
 #include <boost/regex.hpp>
 #include <boost/lexical_cast.hpp>
-
-#include <mutex>
-
-//#include <rsc/misc/SignalWaiter.h>
-//#include <rsc/threading/PeriodicTask.h>
-//#include <rsc/threading/ThreadedTaskExecutor.h>
-#include <rsc/threading/SynchronizedQueue.h>
-#include <rsb/util/QueuePushHandler.h>
-
-// RST Proto types
 #include <rst/geometry/Pose.pb.h>
 #include <rst/geometry/Translation.pb.h>
 #include <rst/geometry/Rotation.pb.h>
 
+#include <mutex>
+#include <thread>
+
+
 using namespace rsb;
 using namespace rsb::converter;
 
-static std::string rsbScope;
-static std::string rsbSubscope;
-static std::string rsbContent;
-
-ros::Publisher vis_pub;
-
+ros::Publisher vizPub;
 tf::TransformListener *listener;
-
 std::vector<std::string> robotNames;
-
+static std::string expectedScope;
+static std::string expectedSubscope;
+static std::string expectedContent;
 static std::size_t markerId = 0;
 std::mutex idMtx;
 double makerRemainTime;
 int markerClass = 0;
-
 double markerScaleX;
 double markerScaleY;
 double markerScaleZ;
@@ -51,52 +39,50 @@ double markerColorR;
 double markerColorG;
 double markerColorB;
 
-void vizScope(rsb::EventPtr msg) {
-  // Get the parent scope
-  ROS_INFO("REC: %s/n", msg->getScope().getComponents().at(0).c_str());
+void vizIt(std::string robots, std::string state, std::string content) {
 
-  // Communication sceme: /robot[1]anotherrobot[3]/[state] -> [Content as string]
-  std::string robots(msg->getScope().getComponents().at(0));
-  std::string state(msg->getScope().getComponents().at(1));
-  std::string content(*boost::static_pointer_cast<std::string>(msg->getData()));
+  // Source and destination names of the robots
+  std::string srcRobotName, dstRobotName;
 
-  // Check subscope
-  if (!rsbSubscope.empty()) {
-    if ( state.compare(rsbSubscope) ) {
-      ROS_INFO("Subscope '%s' does not match '%s'/n", rsbSubscope.c_str(), state.c_str());
+  // Check subscope (don't care if empty)
+  if (!expectedSubscope.empty()) {
+    if ( state.compare(expectedSubscope) != 0 ) {
+      ROS_INFO("Subscope '%s' does not match '%s'/n", expectedSubscope.c_str(), state.c_str());
+//      std::cerr << "Subscope " << expectedSubscope.c_str() <<  " does not match " <<  state.c_str() << std::endl;;
+      return;
     }
   }
 
-  // Check content
-  if (!content.empty()) {
-    if ( content.compare(rsbSubscope) ) {
-      ROS_INFO("Content '%s' does not match '%s'/n", rsbContent.c_str(), content.c_str());
+  // Check content (don't care if empty)
+  if (!expectedContent.empty()) {
+    if ( content.compare(expectedContent) != 0 ) {
+      ROS_INFO("Content '%s' does not match '%s'/n", expectedContent.c_str(), content.c_str());
+//      std::cerr << "Content " << expectedContent.c_str() <<  " does not match " <<  content.c_str() << std::endl;;
+      return;
     }
   }
-
-  std::string src, dst;
 
   // Get the origin
   for ( int idx = 0; idx < robotNames.size(); ++idx) {
-    boost::regex srcRegex(std::string("([^ ]*)") + robotNames.at(idx) + std::string("([0-9]*)"));
+    boost::regex srcRobotNameRegex(std::string("([^ ]*)") + robotNames.at(idx) + std::string("([0-9]*)"));
     boost::cmatch charMatches;
-    if (boost::regex_match(robots.c_str(), charMatches, srcRegex)) {
+    if (boost::regex_match(robots.c_str(), charMatches, srcRobotNameRegex)) {
       // Copy the found string
-      src = charMatches[1].str();
+      srcRobotName = charMatches[1].str();
       for (int idy = 0; idy < charMatches.size(); ++idy) {
         ROS_DEBUG("match: %s\n", charMatches[idy].str().c_str());
       }
     }
   }
   // The rest must be the the destination
-  dst = std::string(&robots.c_str()[src.size()]);
+  dstRobotName = std::string(&robots.c_str()[srcRobotName.size()]);
 
-  ROS_INFO("src robot: %s/n", src.c_str());
-  ROS_INFO("dst robot: %s/n", dst.c_str());
+  ROS_INFO("srcRobotName robot: %s/n", srcRobotName.c_str());
+  ROS_INFO("dstRobotName robot: %s/n", dstRobotName.c_str());
 
   tf::StampedTransform transform;
   try{
-    listener->lookupTransform(dst + std::string("/base_link"), src + std::string("/base_link"), ros::Time(0), transform);
+    listener->lookupTransform(dstRobotName + std::string("/base_link"), srcRobotName + std::string("/base_link"), ros::Time(0), transform);
   }
   catch (tf::TransformException &ex) {
     ROS_ERROR("%s",ex.what());
@@ -104,7 +90,7 @@ void vizScope(rsb::EventPtr msg) {
   }
 
   visualization_msgs::Marker marker;
-  marker.header.frame_id = src + std::string("/base_link");
+  marker.header.frame_id = srcRobotName + std::string("/base_link");
   marker.header.stamp = ros::Time();
   marker.ns = "";
   idMtx.lock();
@@ -118,21 +104,14 @@ void vizScope(rsb::EventPtr msg) {
   // Configure the marker
   if (markerClass == visualization_msgs::Marker::ARROW) {
     marker.type = visualization_msgs::Marker::ARROW;
-    geometry_msgs::Point child, parent;
-//    parent.x = 0.0;
-//    parent.y = 0.0;
-//    parent.z = 0.0;
+    geometry_msgs::Point child, parent /* Is alread (0,0,0)*/;
     child.x = -transform.getOrigin().getX();
     child.y = -transform.getOrigin().getY();
     child.z = -transform.getOrigin().getZ();
     marker.points.push_back(parent);
     marker.points.push_back(child);
   } else if (markerClass == visualization_msgs::Marker::SPHERE) {
-//    child.x = -transform.getOrigin().getX();
-//    child.y = -transform.getOrigin().getY();
-//    child.z = -transform.getOrigin().getZ();
-//    marker.points.push_back(parent);
-//    marker.points.push_back(child);
+    marker.type = visualization_msgs::Marker::SPHERE;
   } else {
     ROS_ERROR("No suitable marker class for id %d. See http://wiki.ros.org/rviz/DisplayTypes/Marker", markerClass);
     return;
@@ -156,15 +135,27 @@ void vizScope(rsb::EventPtr msg) {
 
   // Create the marker
   marker.action = visualization_msgs::Marker::ADD;
-  vis_pub.publish(marker);
+  vizPub.publish(marker);
 
   if (makerRemainTime > 0.0) {
     sleep(makerRemainTime);
     // Delete the marker
     marker.action = visualization_msgs::Marker::DELETE;
-    vis_pub.publish(marker);
+    vizPub.publish(marker);
   }
+}
 
+void vizScope(rsb::EventPtr msg) {
+  // Get the parent scope
+  ROS_INFO("REC: %s/n", msg->getScope().getComponents().at(0).c_str());
+
+  // Communication sceme: /robot[1]anotherrobot[3]/[state] -> [Content as string]
+  std::string robots(msg->getScope().getComponents().at(0));
+  std::string state(msg->getScope().getComponents().at(1));
+  std::string content(*boost::static_pointer_cast<std::string>(msg->getData()));
+
+  std::thread t(vizIt, robots, state, content);
+  t.detach();
 }
 
 int main(int argc, char **argv)
@@ -173,9 +164,9 @@ int main(int argc, char **argv)
   ros::NodeHandle n("~");
 
   // ROS STUFF
-  n.param<std::string>("rsb_viz_scope", rsbScope, "/amiro1tobi");
-  n.param<std::string>("rsb_viz_subscope", rsbSubscope, "");
-  n.param<std::string>("rsb_viz_content", rsbContent, "");
+  n.param<std::string>("rsb_viz_scope", expectedScope, "/amiro1tobi");
+  n.param<std::string>("rsb_viz_subscope", expectedSubscope, "");
+  n.param<std::string>("rsb_viz_content", expectedContent, "");
   n.param<int>("marker_class", markerClass, 0);
   n.param<double>("marker_remain_time", makerRemainTime, 1.0);
   n.param<double>("marker_scale_x", markerScaleX, 0.03);
@@ -187,20 +178,22 @@ int main(int argc, char **argv)
   n.param<double>("marker_color_b", markerColorB, 0.0);
 
   listener = new tf::TransformListener;
-  // RSB STUFF
-  // Get the RSB factory
-  rsb::Factory& factory = rsb::getFactory();
 
+  // Define possible names which may appear during communication
   robotNames.push_back(std::string("amiro"));
   robotNames.push_back(std::string("tobi"));
   robotNames.push_back(std::string("meka"));
 
+  // RSB STUFF
+  // Get the RSB factory
+  rsb::Factory& factory = rsb::getFactory();
+
   // Prepare RSB listener for scopes
-  rsb::ListenerPtr listener = factory.createListener(rsbScope);
+  rsb::ListenerPtr listener = factory.createListener(expectedScope);
   listener->addHandler(rsb::HandlerPtr(new rsb::EventFunctionHandler(&vizScope)));
 
   // ROS publisher for the marker
-  vis_pub = n.advertise<visualization_msgs::Marker>( "visualization_marker", 0 );
+  vizPub = n.advertise<visualization_msgs::Marker>( "visualization_marker", 0 );
 
   ros::spin();
   return 0;
